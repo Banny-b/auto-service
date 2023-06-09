8f9d4f6c9d6a1a9fa4f988991fdc40e7cbc36392
Sergey Petukhov <banny75@i.ua>
2023-06-10 00:17:40 +0300

    auto-service
    fixed mobile

--- js/scrollTop.js
+++ js/scrollTop.js
@@ -4,13 +4,13 @@ window.onscroll = function() {
 
 function scrollFunction() {
     const scrollButton = document.querySelector(".scrollButton");
-    if (window.innerWidth >= 480 && window.innerWidth <= 1199) {
-        scrollButton.classList.remove("show"); // Удаляем класс .show, чтобы скрыть кнопку
+    if (window.innerWidth >= 360 && window.innerWidth <= 1199) {
+        scrollButton.classList.remove("show"); // Removing the .show class to hide the button
     } else {
         if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
-            scrollButton.classList.add("show"); // Добавляем класс .show, чтобы показать кнопку
+            scrollButton.classList.add("show"); // Adding the .show class to show the button
         } else {
-            scrollButton.classList.remove("show"); // Удаляем класс .show, чтобы скрыть кнопку
+            scrollButton.classList.remove("show"); // Removing the .show class to hide the button
         }
     }
 }
@@ -19,26 +19,3 @@ function scrollToTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
-
-
-// window.onscroll = function() {
-//     scrollFunction();
-// };
-
-// function scrollFunction() {
-//     const scrollButtons = document.getElementsByClassName("scrollButton");
-//     if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
-//         for (let i = 0; i < scrollButtons.length; i++) {
-//         scrollButtons[i].style.display = "block";
-//         }
-//     } else {
-//         for (let i = 0; i < scrollButtons.length; i++) {
-//         scrollButtons[i].style.display = "none";
-//         }
-//     }
-// };
-
-// function scrollToTop() {
-//     document.body.scrollTop = 0; // Для Safari
-//     document.documentElement.scrollTop = 0; // Для Chrome, Firefox, IE и Opera
-// };
